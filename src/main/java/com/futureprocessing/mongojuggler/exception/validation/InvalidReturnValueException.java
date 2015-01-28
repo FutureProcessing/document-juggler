0520609753e23e19f9769ff41b5bf0cae21bd277
Bartosz Niziołek <bniziolek@future-processing.com>
2015-01-28 14:36:15 +0100

    Fix code after CR

--- src/main/java/com/futureprocessing/mongojuggler/exception/validation/InvalidReturnValueException.java
+++ src/main/java/com/futureprocessing/mongojuggler/exception/validation/InvalidReturnValueException.java
@@ -3,12 +3,14 @@ package com.futureprocessing.mongojuggler.exception.validation;
 
 import java.lang.reflect.Method;
 
+import static java.lang.String.format;
+
 public class InvalidReturnValueException extends InvalidModelInterfaceException {
 
     private final Method method;
 
     public InvalidReturnValueException(Method method) {
-        super(String.format("Method %s return %s, only String or void are allowed", method, method.getReturnType().getName()));
+        super(format("Method %s return %s, only String or void are allowed", method, method.getReturnType().getName()));
         this.method = method;
     }
 

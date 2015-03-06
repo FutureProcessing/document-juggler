d50958ef1b2eb909d253fc7911a7b75cb96b0189
Szymon Sobocik <ssobocik@future-processing.com>
2015-03-06 12:39:14 +0100

    Removing fields in DB for null and false values is driven with UnsetIfFalse and UnsetIfNull annotations.

--- src/main/java/com/futureprocessing/mongojuggler/update/command/BasicUpdateCommand.java
+++ src/main/java/com/futureprocessing/mongojuggler/update/command/BasicUpdateCommand.java
@@ -5,13 +5,21 @@ import com.futureprocessing.mongojuggler.update.UpdateBuilder;
 
 public class BasicUpdateCommand extends AbstractUpdateCommand {
 
-    public BasicUpdateCommand(String field) {
+    private final boolean unsetIfNull;
+
+    public BasicUpdateCommand(String field, boolean unsetIfNull) {
         super(field);
+        this.unsetIfNull = unsetIfNull;
     }
 
     @Override
     public void update(UpdateBuilder updateBuilder, Object[] args) {
         Object value = args[0];
+
+        if (unsetIfNull && value == null) {
+            updateBuilder.unset(field);
+            return;
+        }
 
         updateBuilder.set(field, value);
     }

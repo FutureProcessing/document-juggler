561c983e5d9fad26173b899a646695722a32d52b
Bartosz Niziołek <bniziolek@future-processing.com>
2015-03-19 11:52:27 +0100

    Extend forbidden check

--- src/main/java/com/futureprocessing/documentjuggler/commons/ForbiddenChecker.java
+++ src/main/java/com/futureprocessing/documentjuggler/commons/ForbiddenChecker.java
@@ -4,6 +4,7 @@ import com.futureprocessing.documentjuggler.Context;
 import com.futureprocessing.documentjuggler.annotation.Forbidden;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static com.futureprocessing.documentjuggler.annotation.AnnotationReader.from;
 import static java.util.Arrays.binarySearch;
@@ -11,13 +12,19 @@ import static java.util.Arrays.binarySearch;
 public final class ForbiddenChecker {
 
     public static boolean isForbidden(Method method, Context context) {
-        Forbidden forbidden = from(method).read(Forbidden.class);
+        List<Forbidden> forbidden = from(method).readAll(Forbidden.class);
 
-        if (forbidden == null) {
+        if (forbidden.isEmpty()) {
             return false;
         }
 
-        return binarySearch(forbidden.value(), context) >= 0;
+        for (Forbidden f : forbidden) {
+            if (binarySearch(f.value(), context) >= 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private ForbiddenChecker() {

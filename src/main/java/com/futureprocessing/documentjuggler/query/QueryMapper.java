48d069f101b99846f97d8f46a0b290ccc28ab394
pcmiel <pcmiel@future-processing.com>
2015-03-24 09:12:00 +0100

    #36 Support for in

--- src/main/java/com/futureprocessing/documentjuggler/query/QueryMapper.java
+++ src/main/java/com/futureprocessing/documentjuggler/query/QueryMapper.java
@@ -49,6 +49,10 @@ public class QueryMapper extends Mapper<QueryCommand> {
             return new LessThanEqualQueryCommand(field);
         }
 
+        if (reader.isPresent(In.class)) {
+            return new InQueryCommand(field);
+        }
+
         return new BasicQueryCommand(field);
     }
 

1dd13043c87182758f4b5b81c32e2d96740c486b
Szymon Sobocik <ssobocik@future-processing.com>
2015-03-26 15:19:58 +0100

    Possibility to provide custom equals to models. Fixes #12

--- src/main/java/com/futureprocessing/documentjuggler/read/ReadProxy.java
+++ src/main/java/com/futureprocessing/documentjuggler/read/ReadProxy.java
@@ -1,5 +1,6 @@
 package com.futureprocessing.documentjuggler.read;
 
+import com.futureprocessing.documentjuggler.commons.EqualsProvider;
 import com.futureprocessing.documentjuggler.read.command.ReadCommand;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
@@ -10,6 +11,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import static com.futureprocessing.documentjuggler.commons.EqualsProvider.Creator.fromAnnotation;
+import static com.futureprocessing.documentjuggler.commons.Methods.EQUALS_METHOD;
+import static java.lang.reflect.Proxy.getInvocationHandler;
 import static java.lang.reflect.Proxy.newProxyInstance;
 
 
@@ -19,27 +23,35 @@ public class ReadProxy implements InvocationHandler {
     private final Set<String> queriedFields;
     private final Map<Method, ReadCommand> readCommands;
     private final Map<Method, Object> cache = new HashMap<>();
+    private final EqualsProvider equalsProvider;
 
     @SuppressWarnings("unchecked")
     public static <READER> READER create(Class<READER> readerType, Map<Method, ReadCommand> readCommands,
                                          DBObject dbObject, Set<String> fields) {
         return (READER) newProxyInstance(readerType.getClassLoader(), new Class[]{readerType},
-                                         new ReadProxy(readerType, readCommands, (BasicDBObject) dbObject, fields));
+                new ReadProxy(readerType, readCommands, (BasicDBObject) dbObject, fields));
     }
 
     private ReadProxy(Class readerType, Map<Method, ReadCommand> readCommands, BasicDBObject dbObject,
                       Set<String> queriedFields) {
+        if (dbObject == null) {
+            throw new RuntimeException("Null dbObject");
+        }
+
         this.dbObject = dbObject;
         this.queriedFields = queriedFields;
         this.readCommands = readCommands;
 
-        if (dbObject == null) {
-            throw new RuntimeException("Null dbObject");
-        }
+        this.equalsProvider = fromAnnotation(readerType);
     }
 
     @Override
     public Object invoke(Object object, Method method, Object[] params) throws Throwable {
+
+        if (method.equals(EQUALS_METHOD)) {
+            return areEqual(object, params[0]);
+        }
+
         if (method.getDeclaringClass().equals(Object.class)) {
             return method.invoke(this, params);
         }
@@ -51,6 +63,21 @@ public class ReadProxy implements InvocationHandler {
         Object value = readCommands.get(method).read(dbObject, queriedFields);
         cache.put(method, value);
         return value;
+    }
+
+    private Object areEqual(Object object, Object param) {
+
+        InvocationHandler invocationHandler = getInvocationHandler(param);
+        if (!(invocationHandler instanceof ReadProxy)) {
+            return false;
+        }
+
+        if (equalsProvider != null) {
+            return equalsProvider.areEqual(object, param);
+        }
+
+        ReadProxy otherProxy = (ReadProxy) invocationHandler;
+        return dbObject.equals(otherProxy.dbObject);
     }
 
 }
---
+++ src/test/java/com/futureprocessing/documentjuggler/integration/EqualsIntegrationTest.java
@@ -0,0 +1,135 @@
+package com.futureprocessing.documentjuggler.integration;
+
+import com.futureprocessing.documentjuggler.Repository;
+import com.futureprocessing.documentjuggler.annotation.DbEmbeddedDocument;
+import com.futureprocessing.documentjuggler.annotation.DbField;
+import com.futureprocessing.documentjuggler.annotation.Equals;
+import com.futureprocessing.documentjuggler.commons.EqualsProvider;
+import com.futureprocessing.documentjuggler.model.DefaultModel;
+import com.futureprocessing.documentjuggler.read.EqualsTest;
+import org.junit.Test;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import static com.futureprocessing.documentjuggler.commons.EqualsProvider.BaseEquals.isEqualClass;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EqualsIntegrationTest extends BaseIntegrationTest {
+
+    private interface MovieWithDefaultEquals extends DefaultModel<MovieWithDefaultEquals> {
+        @DbField("title")
+        String getTitle();
+
+        @DbField("title")
+        MovieWithDefaultEquals withTitle(String title);
+
+        @DbField("director")
+        @DbEmbeddedDocument
+        Person getDirector();
+
+        @DbField("director")
+        @DbEmbeddedDocument
+        MovieWithDefaultEquals withDirector(Consumer<Person> director);
+    }
+
+    private interface Person {
+        @DbField("name")
+        String getName();
+
+        @DbField("name")
+        Person withName(String name);
+    }
+
+    @Test
+    public void oneDocumentShouldBeEqualWithDefaultBehaviour() {
+        //given
+        Repository<MovieWithDefaultEquals> repository = new Repository<>(db().getCollection("col"), MovieWithDefaultEquals.class);
+        final String titanicId = repository.insert(m -> m.withTitle("Titanic"));
+
+        //when
+        MovieWithDefaultEquals first = repository.find(m -> m.withId(titanicId)).first();
+        MovieWithDefaultEquals second = repository.find(m -> m.withId(titanicId)).first();
+
+        //then
+        assertThat(first).isEqualTo(second);
+    }
+
+    @Test
+    public void oneDocumentShouldBeEqualWithDefaultBehaviourAndEmbeddedDocument() {
+        //given
+        Repository<MovieWithDefaultEquals> repository = new Repository<>(db().getCollection("col"), MovieWithDefaultEquals.class);
+        final String titanicId = repository.insert(m -> m.withTitle("Titanic")
+                .withDirector(d -> d.withName("Cameron")));
+
+        //when
+        MovieWithDefaultEquals first = repository.find(m -> m.withId(titanicId)).first();
+        MovieWithDefaultEquals second = repository.find(m -> m.withId(titanicId)).first();
+
+        //then
+        assertThat(first).isEqualTo(second);
+        assertThat(first.getDirector().getName()).isEqualTo("Cameron");
+    }
+
+    @Test
+    public void twoDocumentsShouldNotBeEqualWithDefaultBehaviour() {
+        //given
+        Repository<MovieWithDefaultEquals> repository = new Repository<>(db().getCollection("col"), MovieWithDefaultEquals.class);
+        final String titanicId = repository.insert(m -> m.withTitle("Titanic").withDirector(d -> d.withName("Cameron")));
+        final String avatarId = repository.insert(m -> m.withTitle("Avatar").withDirector(d -> d.withName("Cameron")));
+
+        //when
+        MovieWithDefaultEquals first = repository.find(m -> m.withId(titanicId)).first();
+        MovieWithDefaultEquals second = repository.find(m -> m.withId(avatarId)).first();
+
+        //then
+        assertThat(first).isNotEqualTo(second);
+    }
+
+    @Test
+    public void twoDocumentsShouldNotBeEqualWithDefaultBehaviourWhenHaveDifferentId() {
+        //given
+        Repository<MovieWithDefaultEquals> repository = new Repository<>(db().getCollection("col"), MovieWithDefaultEquals.class);
+        final String titanicId = repository.insert(m -> m.withTitle("Titanic").withDirector(d -> d.withName("Cameron")));
+        final String titanicTwoId = repository.insert(m -> m.withTitle("Titanic").withDirector(d -> d.withName("Cameron")));
+
+        //when
+        MovieWithDefaultEquals first = repository.find(m -> m.withId(titanicId)).first();
+        MovieWithDefaultEquals second = repository.find(m -> m.withId(titanicTwoId)).first();
+
+        //then
+        assertThat(first).isNotEqualTo(second);
+    }
+
+    @Equals(TitleEquals.class)
+    private interface MovieWithCustomEquals extends MovieWithDefaultEquals{
+
+    }
+
+    public static class TitleEquals implements EqualsProvider<MovieWithCustomEquals> {
+        @Override
+        public boolean areEqual(MovieWithCustomEquals model, Object obj) {
+            if (!isEqualClass(model, obj)){
+                return false;
+            }
+            MovieWithCustomEquals o = (MovieWithCustomEquals) obj;
+            return Objects.equals(model.getTitle(), o.getTitle());
+        }
+    }
+
+    @Test
+    public void twoDocumentsShouldBeEqualWithCustomEquals() {
+        //given
+        Repository<MovieWithCustomEquals> repository = new Repository<>(db().getCollection("col"), MovieWithCustomEquals.class);
+        final String titanicId = repository.insert(m -> m.withTitle("Titanic").withDirector(d -> d.withName("Cameron")));
+        final String titanicTwoId = repository.insert(m -> m.withTitle("Titanic").withDirector(d -> d.withName("Cameron")));
+
+        //when
+        MovieWithDefaultEquals first = repository.find(m -> m.withId(titanicId)).first();
+        MovieWithDefaultEquals second = repository.find(m -> m.withId(titanicTwoId)).first();
+
+        //then
+        assertThat(first).isEqualTo(second);
+    }
+
+}

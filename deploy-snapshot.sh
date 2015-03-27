2f66aa60c26f1e215cb6faa149a28c887f6b8bf7
Bartosz Niziołek <bniziolek@future-processing.com>
2015-03-27 13:52:54 +0100

    Test branch condition

--- deploy-snapshot.sh
+++ deploy-snapshot.sh
@@ -4,6 +4,6 @@ SNAPSHOT=`mvn help:evaluate -Dexpression=project.version | grep -v '\[' | grep S
 
 echo "$TRAVIS_BRANCH"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "publish-shapshot" -a ! -z "$SNAPSHOT" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "publish-shanpshot" -a ! -z "$SNAPSHOT" ]; then
     mvn deploy -DskipTests=true --settings ./settings.xml
 fi

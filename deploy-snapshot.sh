e56f25600493192b5e6c2005641f655a10e42752
Bartosz Niziołek <bniziolek@future-processing.com>
2015-03-27 13:47:45 +0100

    FIx travis config

--- deploy-snapshot.sh
+++ deploy-snapshot.sh
@@ -2,6 +2,8 @@
 
 SNAPSHOT=`mvn help:evaluate -Dexpression=project.version | grep -v '\[' | grep SNAPSHOT`;
 
+echo "$TRAVIS_BRANCH"
+
 if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "publish-shapshot" -a ! -z "$SNAPSHOT" ]; then
     mvn deploy -DskipTests=true --settings ./settings.xml
 fi
